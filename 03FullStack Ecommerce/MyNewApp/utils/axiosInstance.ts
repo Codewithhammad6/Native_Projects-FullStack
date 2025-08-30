dd9e94d94211906208cb756ba95d1e534ced3a89
Hammad Chaudhary <codewithhammad6@gmail.com>
2025-08-30 18:55:58 +0500

    update

--- 03FullStack Ecommerce/MyNewApp/utils/axiosInstance.ts
+++ 03FullStack Ecommerce/MyNewApp/utils/axiosInstance.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 import { API_URL } from "@env"; 
 const axiosInstance = axios.create({
   // baseURL: "http://10.0.2.2:5000/api",      // when use simulator
-// baseURL: "http://192.168.100.12:5000/api",  // use real device with cable pc ip address 192.168.100.12
-baseURL: API_URL,   // when use backend on production
+  baseURL: "http://192.168.100.12:5000/api",     // use real device with cable pc ip address 192.168.100.12
+ // baseURL: API_URL,                           // when use backend on production
 
   withCredentials: true,
 });
